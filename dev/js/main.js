353cf63fb97125aa5f0f4600abdeccd9e15fa7ca
Narek <narek.tarverdyan@bk.ru>
2017-07-18 15:07:10 +0300

    homepage done

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+$(document).ready(function() {
+	$('.open-menu').click(function(event) {
+		event.preventDefault();
+		$('.main-sidebar').addClass('visible');
+	});
+	$('.main-sidebar__close').click(function(event) {
+		event.preventDefault();
+		$('.main-sidebar').removeClass('visible');
+	});
+});
