1c9517f38bd93e21c5cd0b999aa6df2335081ef8
Narek <narek.tarverdyan@bk.ru>
2017-07-19 16:05:49 +0300

    contacts page

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,4 @@
+var PizzaThemeGlobalVariables = {};
 $(document).ready(function() {
 	$('.open-menu').click(function(event) {
 		event.preventDefault();
@@ -7,4 +8,32 @@ $(document).ready(function() {
 		event.preventDefault();
 		$('.main-sidebar').removeClass('visible');
 	});
+	function setSelectWidth() {
+		$('.custom-select__wrapper').each(function(index, el) {
+			$(this).find(".custom-select--compute-value").html($(this).find('.custom-select option:selected').text());
+			$(this).find("select.custom-select").width($(this).find(".custom-select--compute").width());
+		});
+	}
+	PizzaThemeGlobalVariables.changeSelectWidthOnChange =  function(el)  {
+		var container = $(el).parents('.custom-select__wrapper');
+		var text = $(container).find('.custom-select option:selected').text();
+		$(container).find(".custom-select--compute-value").html(text);
+		var width = $(container).find(".custom-select--compute").width();
+		$(container).find("select.custom-select").width(width);
+	};
+	setSelectWidth()
+	$('.custom-select').change(function(event) {
+		PizzaThemeGlobalVariables.changeSelectWidthOnChange($(this));
+	});
+	$('.district-change__select').on('change', function() {
+		var selectedDistrict = $(this).val();
+		if(selectedDistrict == 'all') {
+			$('.contacts__sidebar-list-item').removeClass('unvisible');
+		}else {
+			$('.contacts__sidebar-list-item').removeClass('unvisible');
+			$('.contacts__sidebar-list-item').not(' [data-district=' + selectedDistrict +'] ').addClass('unvisible');
+		}
+	});
+
+
 });
